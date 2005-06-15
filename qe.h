240b6edfd059e7ec8abe864ca39d328b19af07c7
Charlie Gordon <qemacs@chqrlie.org>
2005-06-15 16:36:45 +0000

    applied patch by Guido Classen: use the new attribute __used__ for the initcall/exitcall sections to prevent gcc 3.4 and above from droping contents of this sections.

--- qe.h
+++ qe.h
@@ -627,15 +627,16 @@ extern EditBufferDataType raw_data_type;
 /* make sure that the keyword is not disabled by glibc (TINYC case) */
 #undef __attribute__ 
 
-#ifndef __attribute_used__
-#define __attribute_used__
-#endif
-
+#if __GNUC__ < 3 || (__GNUC__ == 3 && __GNUC_MINOR__ < 3)
 /* same method as the linux kernel... */
-#define __init_call     __attribute_used__ \
-                        __attribute__((unused, __section__ (".initcall.init")))
-#define __exit_call     __attribute_used__ \
-                        __attribute__((unused, __section__ (".exitcall.exit")))
+#define __init_call     __attribute__((unused, __section__ (".initcall.init")))
+#define __exit_call     __attribute__((unused, __section__ (".exitcall.exit")))
+#else
+#undef __attribute_used__
+#define __attribute_used__	__attribute__((__used__))
+#define __init_call	__attribute_used__ __attribute__((__section__ (".initcall.init")))
+#define __exit_call	__attribute_used__ __attribute__((__section__ (".exitcall.exit")))
+#endif
 
 #define qe_module_init(fn) \
         static int (*__initcall_##fn)(void) __init_call = fn
