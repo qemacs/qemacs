1e81e379c68290f4ff6134100fefacd1ab2e8dfb
Charlie Gordon <qemacs@chqrlie.org>
2014-08-09 17:31:50 +0000

    colorize lsl and lsr addressing operators

--- arm.c
+++ arm.c
@@ -21,7 +21,7 @@
 #include "qe.h"
 
 char const arm_registers[] = {
-    "r0|r1|r2|r3|r4|r5|r6|r7|r8|r9|r10|r11|r12|r13|r14|r15|lr|sp|pc|cpsr|spsr|"
+    "r0|r1|r2|r3|r4|r5|r6|r7|r8|r9|r10|r11|r12|r13|r14|r15|lr|sp|pc|cpsr|spsr|lsl|lsr|"
 };
 
 /*---------------- ARM Assembly language coloring ----------------*/
