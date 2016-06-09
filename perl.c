d5e10dfe77a4864bac8af8251f24d709e7ad0f5a
Charlie Gordon <qemacs@chqrlie.org>
2016-06-09 16:24:10 +0000

    update copyrights, TODO list

--- perl.c
+++ perl.c
@@ -1,7 +1,7 @@
 /*
  * Perl Source mode for QEmacs.
  *
- * Copyright (c) 2000-2014 Charlie Gordon.
+ * Copyright (c) 2000-2016 Charlie Gordon.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
