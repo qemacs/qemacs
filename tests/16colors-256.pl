1fcec4b9ac80f50faa834cfe916d619917415ba7
Charlie Gordon <qemacs@chqrlie.org>
2018-12-19 11:48:29 +0000

    add some test files

---
+++ tests/16colors-256.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl
+#
+# Copyright (c) 2002-2017 Charlie Gordon.
+#
+# use the resources for colors 0-15 - usually more-or-less a
+# reproduction of the standard ANSI colors, but possibly more
+# pleasing shades
+
+# display the colors
+
+# first the system ones:
+print "Background colors:\n";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[48;5;%dm %02d ", $color, $color);
+    # print "\x1b[${n}m    ";
+}
+print "\x1b[0m\n\n";
+
+print "Blinking colors:\n";
+print "\x1b[5m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[48;5;%dm %02d ", $color, $color);
+    # print "\x1b[${n}m    ";
+}
+print "\x1b[0m\n\n";
+
+print "Foreground colors:\n";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Bold colors:\n";
+print "\x1b[1m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Italic colors:\n";
+print "\x1b[3m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Bold+Italic colors:\n";
+print "\x1b[1m\x1b[3m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Underline colors:\n";
+print "\x1b[4m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Reverse colors:\n";
+print "\x1b[7m";
+for ($color = 0; $color < 16; $color++) {
+    printf("\x1b[38;5;%dm %02d ", $color, $color);
+}
+print "\x1b[0m\n\n";
+
+print "Color combinations:\n";
+for ($bg = 0; $bg < 16; $bg++) {
+    printf("\x1b[48;5;%dm", $bg);
+    # printf("\x1b[%dm", ($color > 7) ? 5 : 25);
+    # printf("\x1b[%dm", 40 + ($color & 7));
+    for ($fg = 0; $fg < 16; $fg++) {
+        printf("\x1b[38;5;%dm %02d ", $fg, $fg);
+        # printf("\x1b[%dm", ($fg > 7) ? 1 : 22);
+        # printf("\x1b[%dm %02d ", 30 + ($fg & 7), $fg);
+    }
+    print "\x1b[0m\n";
+}
+print "\x1b[0m\n";
---
+++ tests/altcharset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# '<' '%%' '>' are VT220 only
+for x in '0' ; do  
+  if [ "$1" == "--ascii" ] ; then
+    # ASCII version
+    printf "+-----------------------------------------------------------------------------+\n"
+    printf "|  VT100 line drawing character set table using ^[(B^[)$x ^N and ^O sequences  |\n"
+    printf "+-----------------------------------------------------------------------------+\n"
+    for i in {0..15} ; do
+      for j in 32 48 64 80 96 112 ; do
+        n=$[$i+$j]
+        c=$(printf "\\%03o" $n)
+        printf "| %02X | $c | \016$c\017 " $[i+j]
+      done
+      printf "|\n"
+    done
+    printf "+-----------------------------------------------------------------------------+\n\n"
+  else
+    # Using VT100 line drawing characters
+    printf "\033(B\033)$x"
+    printf "\016lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk\017\n"
+    printf "\016x\017  VT100 line drawing character set table using ^[(B^[)$x ^N and ^O sequences  \016x\017\n"
+    printf "\016tqqqqwqqqwqqqwqqqqwqqqwqqqwqqqqwqqqwqqqwqqqqwqqqwqqqwqqqqwqqqwqqqwqqqqwqqqwqqqu\017\n"
+    for i in {0..15} ; do
+      for j in 32 48 64 80 96 112 ; do
+        n=$[$i+$j]
+        c=$(printf "\\%03o" $n)
+        printf "\016x\017 %02X \016x\017 $c \016x\017 \016$c\017 " $n
+      done
+      printf "\016x\017\n"
+    done
+    printf "\016mqqqqvqqqvqqqvqqqqvqqqvqqqvqqqqvqqqvqqqvqqqqvqqqvqqqvqqqqvqqqvqqqvqqqqvqqqvqqqj\017\n"
+  fi
+done
