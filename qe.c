e08de03499cf638c44bd428f0532b0d4307c54aa
Charlie Gordon <qemacs@chqrlie.org>
2022-08-03 17:00:38 +0200

    updated credits and copyrights

--- qe.c
+++ qe.c
@@ -8853,7 +8853,7 @@ void qe_register_cmd_line_options(CmdLineOptionDef *table)
 
 const char str_version[] = "QEmacs version " QE_VERSION;
 const char str_credits[] = "Copyright (c) 2000-2003 Fabrice Bellard\n"
-                           "Copyright (c) 2000-2020 Charlie Gordon\n";
+                           "Copyright (c) 2000-2022 Charlie Gordon\n";
 
 static void show_version(void)
 {
